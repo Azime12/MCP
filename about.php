6e93f44d6cc3af436ecbeeb88303c733cf66e654
belayneh <betenatefera@gmail.com>
2024-01-10 20:57:15 +0300

    about page modified

--- about.php
+++ about.php
@@ -22,28 +22,29 @@
             <div class="col-md-6">
                 <img class="img-responsive" src="images/collaboration.png" alt="">
             </div>
-            <div class="col-md-6">
-                <h1>Founders</h1>
-                        <ul>
-                            <li>Azimerw taddese</li>
-                            <li>Bahiru yimolal</li>
-                            <li>Banteamlak Gezahegn</li>
-                            <li>Bedane Deriba</li>
-                            <li>Belayneh Getachew</li>
-                        </ul>
-                <p></p>
-            </div>
+<div class="col-md-6" style="height: 350px; border: .5px solid #333; padding: 15px; background-color: #f0f0f0; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);">
+    <h1 style="color: #658165;">Founders</h1>
+    <ul style="list-style: none; padding: 0;">
+        <li style="color: #555;">Azimerw Taddese</li>
+        <li style="color: #555;">Bahiru Yimolal</li>
+        <li style="color: #555;">Banteamlak Gezahegn</li>
+        <li style="color: #555;">Bedane Deriba</li>
+        <li style="color: #555;">Belayneh Getachew</li>
+    </ul>
+</div>
+
+
         </div>
         <!-- /.row -->
 
         <!-- Team Members -->
         <div class="row">
-            <div class="col-lg-12">
-                <h2 class="page-header">Some of our Consultants</h2>
+            <div class="col-lg-12 text-center">
+                <h2 class="page-header" style="color: #728d71;">Some of our Consultants</h2>
             </div>
             <div class="col-md-4 text-center">
                 <div class="thumbnail">
-                    <img class="img-responsive" src="images/253f9ac.jpg" alt="">
+                    <img class="img-responsive" src="images/253f9ac.jpg" alt="" style="transition: transform 0.8s; background-color: #ffda;">
                     <div class="caption">
                         <h3>Professor tesfaye shemela<br>
                             <small>Dean</small>
@@ -62,7 +63,7 @@
             </div>
             <div class="col-md-4 text-center">
                 <div class="thumbnail">
-                    <img class="img-responsive" src="images/sif10225.225782846266003821356.jpg" alt="">
+                    <img class="img-responsive" src="images/sif10225.225782846266003821356.jpg" alt=""style="transition: transform 0.8s;">
                     <div class="caption">
                         <h3>Professor birhanu nega<br>
                             <small>Leader</small>
@@ -81,7 +82,7 @@
             </div>
             <div class="col-md-4 text-center">
                 <div class="thumbnail">
-                    <img class="img-responsive" src="images/Lærer-studerende-på-UCN.jpg" alt="">
+                    <img class="img-responsive" src="images/Lærer-studerende-på-UCN.jpg" alt=""style="transition: transform 0.8s;">
                     <div class="caption">
                         <h3>Professor hirut woldemaryam<br>
                             <small>Professor</small>
@@ -100,7 +101,7 @@
             </div>
            . <div class="col-md-4 text-center">
                 <div class="thumbnail">
-                    <img class="img-responsive" src="images/teacher2.jpg" alt="">
+                    <img class="img-responsive" src="images/teacher2.jpg" alt=""style="transition: transform 0.8s;">
                     <div class="caption">
                         <h3>professor tasew weldehana<br>
                             <small>professor</small>
@@ -119,7 +120,7 @@
             </div>
             <div class="col-md-4 text-center">
                 <div class="thumbnail">
-                    <img class="img-responsive" src="images/teacher222223.jpg" alt="">
+                    <img class="img-responsive" src="images/teacher222223.jpg" alt=""style="transition: transform 0.8s;">
                     <div class="caption">
                         <h3>Professor gebre yntiso<br>
                             <small>associate professor</small>
@@ -138,7 +139,7 @@
             </div>
             <div class="col-md-4 text-center">
                 <div class="thumbnail">
-                    <img class="img-responsive" src="images/20160518144520.jpg" alt="">
+                    <img class="img-responsive" src="images/20160518144520.jpg" alt=""style="transition: transform 0.8s;">
                     <div class="caption">
                         <h3>Professor woldamlak bewketu<br>
                             <small>professor</small>
@@ -160,39 +161,127 @@
 
         <!-- Our Customers -->
         <div class="row">
-            <div class="col-lg-12">
-                <h2 class="page-header">Some of students that work with us</h2>
+            <div class="col-lg-12 text-center">
+                <h2 class="page-header" style="color: #6f966e;" >Some of students that work with us</h2>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6">
-                <img class="img-responsive customer-img" src="resources/images/banners/istockphoto-490486272-612x612.jpg" alt="">
-                Hana Leta<br>            
-                student in AAu
+            
+
+            <div class="col-md-4 text-center">
+    <div class="thumbnail">
+        <img class="img-responsive" src="images/smilingFemaleStudent.jpg" alt="" style="transition: transform 0.8s;">
+        <div class="caption">
+            <h3>Hana Leta</h3><br>
+            <p>Student in AAU</p>
+            <ul class="list-inline">
+                <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a></li>
+                <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a></li>
+                <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="images/banners/pexels-edmond-dantès-4347366.jpg" alt=""style="transition: transform 0.8s;">
+                    <div class="caption">
+                        <h3>Hewan Kora
+                    </h3><br>
+                            <p>Student in Jimma university</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6">
-                <img class="img-responsive customer-img" src="images/smilingFemaleStudent.jpg" alt="">
-                Hewan Kora<br>            
-                student in Jimma university
+
+
+
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="images/Returning-Student.jpg" alt=""style="transition: transform 0.8s;">
+                    <div class="caption">
+                        <h3>Hasset Desta
+                    </h3><br>
+                    <p>Student in AASTU</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6">
-                <img class="img-responsive customer-img" src="images/Returning-Student.jpg" alt="">
-                Haset Desta<br>            
-                student in ASTU
+
+
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="images/library_guy_900x400.jpg" alt=""style="transition: transform 0.8s;">
+                    <div class="caption">
+                        <h3>Birhanu Tefera
+                    </h3><br>
+                    <p>Student in Gonder University</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6">
-                <img class="img-responsive customer-img" src="images/Hopeful_student_1.jpg" alt="">
-                Hermon Leul<br>            
-                student in AASTU
+            
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="images/Hopeful_student_1.jpg" alt=""style="transition: transform 0.8s;">
+                    <div class="caption">
+                        <h3>Hermon Leul
+                    </h3><br>
+                    <p>Student in ASTU</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6">
-                <img class="img-responsive customer-img" src="images/happy_student4.jpg" alt="">
-                Biru Wake<br>            
-                student in Gonder university
+
+
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="images/happy_student4.jpg" alt="" style="transition: transform 0.8s;">
+                    <div class="caption">
+                        <h3>Biru Wake
+                    </h3><br>
+                    <p>Student in BDU</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6">
-                <img class="img-responsive customer-img" src="images/library_guy_900x400.jpg" alt="">
-                Dani Leta<br>            
-                student in BDU
-            </div>
+
         </div>
         <!-- /.row -->
 
@@ -200,7 +289,7 @@
         <!-- Footer -->
         <footer>
           
-                    <p class="footer text-center">Copyright &copy; Matric and College Preparation System 2017  <strong>BCA</strong> project Made by <strong>VISHAL MATHUR 146891645</strong></p>
+                    <p class="footer text-center">Copyright &copy; Matric and College Preparation System 2022  <strong>BCA</strong> project Made by <strong>AASTU software engineering students</strong></p>
           
         </footer>
 
@@ -209,7 +298,9 @@
 <style>
 
 .footer{background: #000; padding: 10px 0px; color: #fff;position: fixed;left: 0; right: 0;bottom: -10px;}
-
+.thumbnail:hover img {
+    transform: translateY(-10px); /* Adjust the value to control the amount of lift */
+}
 </style>
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
